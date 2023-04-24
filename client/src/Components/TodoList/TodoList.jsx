04a15c5d98ff56af19b3695b2605218ed6ac914b
vanshita shah <vanshita.s@simformsolutions.com>
2023-04-24 18:41:22 +0530

    Background image for no task display is added

--- client/src/Components/TodoList/TodoList.jsx
+++ client/src/Components/TodoList/TodoList.jsx
@@ -2,23 +2,16 @@
 import React from 'react'
 import CheckedTask from '../Task/CheckedTask'
 import UncheckedTask from '../Task/UncheckedTask'
+import NoTodo from '../../Assets/No_Task.png'
+
+{ console.log(NoTodo) }
 //Imported style module 
 import ListStyle from './TodoList.module.css'
 const TodoList = () => {
     return (
         <div className={ListStyle.listContainer}>
             {/* Tasks */}
-            <CheckedTask />
-            <CheckedTask />
-            <CheckedTask />
-            <UncheckedTask />
-            <UncheckedTask />
-            <UncheckedTask />
-            <UncheckedTask />
-            <UncheckedTask />
-            <UncheckedTask />
-            <UncheckedTask />
-            <UncheckedTask />
+            <img src={NoTodo} alt="a" width={250} />
         </div>
     )
 }
